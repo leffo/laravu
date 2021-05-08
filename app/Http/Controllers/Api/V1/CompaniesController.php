453489242e13dae0bd8da9d172765070763aee73
Alexandr Yakovlev <leffalt@gmail.com>
2021-05-08 12:30:17 +0700

    amend

---
+++ app/Http/Controllers/Api/V1/CompaniesController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Company;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CompaniesController extends Controller
+{
+    public function index()
+    {
+        //
+        return Company::all();
+    }
+
+    public function show($id)
+    {
+        //
+        return Company::findOrFail($id);
+    }
+
+    public function update(Request $request, $id)
+    {
+        //
+        $company = Company::findOrFail($id);
+    }
+
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+   /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
