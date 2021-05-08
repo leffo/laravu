3940ce1dcd739157ef18fe6ff24dd89b09e121b8
Alexandr Yakovlev <leffalt@gmail.com>
2021-05-08 12:35:32 +0700

    add ide-helper

--- app/Company.php
+++ app/Company.php
@@ -4,6 +4,14 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Company
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|Company newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Company newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Company query()
+ * @mixin \Eloquent
+ */
 class Company extends Model
 {
     // поля доступные для заполнения
